ad218738cf46c3ec3558a817a7b49864b16d25f2
raineduc <hramil0120@gmail.com>
2021-12-04 14:59:15 +0300

    Init project

---
+++ src/main/java/com/example/kafkainternapp/KafkaInternAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkainternapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInternAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaInternAppApplication.class, args);
+    }
+
+}
