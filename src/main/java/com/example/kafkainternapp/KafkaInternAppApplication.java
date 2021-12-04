5128e005ad4edf36e7329e9adfedf957eceba028
raineduc <hramil0120@gmail.com>
2021-12-04 15:20:07 +0300

    Configure docker and docker compose with PostgreSQL database

--- src/main/java/com/example/kafkainternapp/KafkaInternAppApplication.java
+++ src/main/java/com/example/kafkainternapp/KafkaInternAppApplication.java
@@ -1,13 +1,18 @@
 package com.example.kafkainternapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaInternAppApplication {
+public class KafkaInternAppApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaInternAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Kafka app");
+    }
 }
